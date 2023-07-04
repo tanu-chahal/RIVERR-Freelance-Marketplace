1aa05a161d449c32feb82d15939f9856f88b1e65
Tanu Chahal <tanuchahal0004@gmail.com>
2023-07-04 12:04:44 +0530

    Fixed mark as read bug on a seller profile.

--- client/src/pages/messages/Messages.jsx
+++ client/src/pages/messages/Messages.jsx
@@ -85,7 +85,7 @@ const Messages = () =>{
                         <td>{userNames[index]}</td>
                         <td><Link to={`/message/${c.id}`} className='link'>{c?.lastMessage?.substring(0,90)}...</Link></td>
                         <td>{moment(c.updatedAt).fromNow()}</td>
-                        <td>{(currentUser.isSeller && !c.readBySeller) || (!currentUser.isSeller && !c.readByBuyer) &&
+                        <td>{((currentUser.isSeller && !c.readBySeller) || (!currentUser.isSeller && !c.readByBuyer))&&
                         (<button onClick={()=>handleRead(c.id)}>Mark as Read</button>)}</td>
                     </tr>)
                     })
