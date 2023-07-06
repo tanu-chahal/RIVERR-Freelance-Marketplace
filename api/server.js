2113b85276b565d5a616abdf303ab502645aab44
Tanu Chahal <tanuchahal0004@gmail.com>
2023-07-06 18:07:34 +0530

    Updated live urls

--- api/server.js
+++ api/server.js
@@ -23,7 +23,7 @@ const connect = async () => {
     console.log(error);
   }
 };
-const allowedOrigins = ["http://127.0.0.1:5173", "https://master--amazing-tarsier-1b80f4.netlify.app/"]
+const allowedOrigins = ["http://127.0.0.1:5173", "https://riverr-freelance.netlify.app/"]
 
 app.use(cors({ origin: allowedOrigins , credentials: true })); //to allow multiple websites (like our frontend) to interact with our api
 app.use(express.json()); //a middleware to let our application take input from the user
